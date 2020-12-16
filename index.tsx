af956a8da26967e819cf72c32c370a9b53b5ae00
Seemann <mail@sannybuilder.com>
2020-12-15 22:32:18 -0500

    Update index

--- index.tsx
+++ index.tsx
@@ -1,5 +1,11 @@
-<>(
-    <div class="card-wrapper">
-        <a href="/classes/gta3"><img src="/classes/assets/gta3.img"/></a>
-    </div>
-)</>
+import { React } from "https://deno.land/x/pagic/mod.ts";
+
+const Page = () => (
+  <div class="card-wrapper">
+    <a href="/classes/gta3">
+      <img src="/classes/assets/gta3.img" />
+    </a>
+  </div>
+);
+
+export default Page;

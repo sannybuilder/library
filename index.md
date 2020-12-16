4c34949d5dcaddaf36fd3207219f21f6b5ee0b23
Seemann <mail@sannybuilder.com>
2020-12-15 22:27:47 -0500

    Update index

--- index.md
+++
@@ -1,3 +0,0 @@
-* [GTA 3 Commands](gta3/index.md)
-* [GTA 3 Classes list](gta3/classes.md)
-
---
+++ index.tsx
@@ -0,0 +1,6 @@
+<>(
+    <div class="card-wrapper">
+        <img src="/classes/assets/gta3.img"/>
+        <a href="/classes/gta3">GTA 3 Classes</a>
+    </div>
+)</>

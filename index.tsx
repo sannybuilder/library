921cf0018304899360608a848e414dd747a3b578
Seemann <mail@sannybuilder.com>
2021-02-13 00:07:15 -0500

    update layout url

--- index.tsx
+++ index.tsx
@@ -4,15 +4,15 @@ const Page = () => (
   <div className="card-wrapper">
     <div className="card">
       <h2>GTA III</h2>
-      <a href="/classes/gta3">
-        <img src="/classes/assets/gta3.jpg" />
+      <a href="/library/gta3">
+        <img src="/library/assets/gta3.jpg" />
       </a>
     </div>
 
     <div className="card">
       <h2>GTA Vice City</h2>
-      <a href="/classes/vc">
-        <img src="/classes/assets/vc.jpg" />
+      <a href="/library/vc">
+        <img src="/library/assets/vc.jpg" />
       </a>
     </div>
   </div>

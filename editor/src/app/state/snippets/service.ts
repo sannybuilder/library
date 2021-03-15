2a7801f7ff871280337a66ebae5c7e09ce6d7a97
Seemann <mail@sannybuilder.com>
2021-03-15 18:42:18 -0400

    update

--- editor/src/app/state/snippets/service.ts
+++ editor/src/app/state/snippets/service.ts
@@ -2,19 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Location } from '@angular/common';
 import { Observable } from 'rxjs';
-import { CONFIG, Config } from '../../config';
 import { ExtensionSnippets, Game, GameSnippets } from '../../models';
 
 @Injectable({ providedIn: 'root' })
 export class SnippetsService {
-  constructor(
-    private http: HttpClient,
-    @Inject(CONFIG) public config: Config
-  ) {}
+  constructor(private http: HttpClient) {}
 
   loadSnippets(game: Game): Observable<ExtensionSnippets> {
     return this.http.get<ExtensionSnippets>(
-      Location.joinWithSlash(this.config.endpoints.base, GameSnippets[game])
+      Location.joinWithSlash('/assets', GameSnippets[game])
     );
   }
 }

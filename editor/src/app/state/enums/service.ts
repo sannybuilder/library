b7ad722a6f3bac4d9497fe43efe082edea0cd4e4
Seemann <mail@sannybuilder.com>
2021-04-24 23:12:17 -0400

    change enums loading path

--- editor/src/app/state/enums/service.ts
+++ editor/src/app/state/enums/service.ts
@@ -1,20 +1,28 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { Location } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { Game, GameEnums, Enums } from '../../models';
 import { catchError } from 'rxjs/operators';
+
+import { Game, GameEnums, Enums } from '../../models';
+import { CONFIG, Config } from '../../config';
 
 @Injectable({ providedIn: 'root' })
 export class EnumsService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(CONFIG) private _config: Config
+  ) {}
 
   loadEnums(game: Game): Observable<Enums> {
     const ts = Date.now().toString();
     return this.http
-      .get<Enums>(Location.joinWithSlash('/assets', GameEnums[game]), {
-        params: { ts },
-      })
+      .get<Enums>(
+        Location.joinWithSlash(this._config.endpoints.base, GameEnums[game]),
+        {
+          params: { ts },
+        }
+      )
       .pipe(catchError(() => of({})));
   }
 }

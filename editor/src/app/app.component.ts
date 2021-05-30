19dfd7955c0822806594cad76668994e507ad9d8
Seemann <mail@sannybuilder.com>
2021-05-30 18:47:42 -0400

    fix slowness on large list scroll

--- editor/src/app/app.component.ts
+++ editor/src/app/app.component.ts
@@ -8,29 +8,38 @@ import { Component, HostListener, Inject } from '@angular/core';
 })
 export class AppComponent {
   private _lastScroll = 0;
+  private _ticking = false;
+
   constructor(@Inject(DOCUMENT) private _d: Document) {}
 
   @HostListener('window:scroll', [])
   onScroll() {
+    if (!this._ticking) {
+      requestAnimationFrame(() => {
+        this.updateBodyClasses();
+        this._ticking = false;
+      });
+
+      this._ticking = true;
+    }
+  }
+
+  private updateBodyClasses() {
     const classList = this._d.body.classList;
     const scrollUp = 'scroll-up';
     const scrollDown = 'scroll-down';
     const scrolledDown = 'scrolled-down';
-
     const currentScroll = window.pageYOffset;
-
     // go to top link
     if (currentScroll > 53) {
       classList.add(scrolledDown);
     } else {
       classList.remove(scrolledDown);
     }
-
     if (currentScroll <= 0) {
       classList.remove(scrollUp);
       return;
     }
-
     if (currentScroll > this._lastScroll && !classList.contains(scrollDown)) {
       classList.remove(scrollUp);
       classList.add(scrollDown);

e890a5292eb9b7ec3e59edd968b642d027a6845f
Seemann <mail@sannybuilder.com>
2021-07-07 14:49:49 -0400

    highlight words in description that match param names

--- editor/src/app/pipes/linkify.pipe.ts
+++ editor/src/app/pipes/linkify.pipe.ts
@@ -1,18 +1,28 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Game } from '../models';
+import { Command, Game } from '../models';
+import { commandParams } from '../utils';
 
 @Pipe({
   name: 'linkify',
 })
 export class LinkifyPipe implements PipeTransform {
-  transform(content: string, game: Game, extension: string) {
+  transform(content: string, game: Game, extension: string, command: Command) {
     if (!content) {
       return content;
     }
 
-    return content.replace(
+    const links = content.replace(
       /0[\dA-Fa-f][\dA-Fa-f][\dA-Fa-f]/g,
       `<a href="#/${game}/${extension}/$&">$&</a>`
     );
+
+    const paramNames = commandParams(command).map((p) => p.name);
+    return paramNames.reduce((m, v) => {
+      if (!v) {
+        return m;
+      }
+      const re = new RegExp(`\\b${v}\\b`, 'gi');
+      return m.replace(re, `<span class="identifier">$&</span>`);
+    }, links);
   }
 }

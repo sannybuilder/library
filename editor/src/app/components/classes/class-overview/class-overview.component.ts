db1d93184f6169ded4a0f4ff74166bdca0b3f8ac
Seemann <mail@sannybuilder.com>
2021-07-07 01:18:32 -0400

    fix class overview display with multiple extensions

--- editor/src/app/components/classes/class-overview/class-overview.component.ts
+++ editor/src/app/components/classes/class-overview/class-overview.component.ts
@@ -8,6 +8,8 @@ import {
 import { UiFacade } from '../../../state';
 import { Command, DEFAULT_EXTENSION, Game } from '../../../models';
 
+type ClassCommand = { command: Command; extension: string };
+
 @Component({
   selector: 'scl-class-overview',
   templateUrl: './class-overview.component.html',
@@ -15,14 +17,28 @@ import { Command, DEFAULT_EXTENSION, Game } from '../../../models';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClassOverviewComponent {
+  private _classCommands: ClassCommand[];
   DEFAULT_EXTENSION = DEFAULT_EXTENSION;
   displayInlineDescription$ = this._ui.displayInlineMethodDescription$;
   filterQuery = '';
+  commandsHaveSameOrigin = true;
 
   @Input() game: Game;
   @Input() className: string;
   @Input() classOrigin: string;
-  @Input() classCommands: Array<{ command: Command; extension: string }>;
+  @Input() set classCommands(val: ClassCommand[]) {
+    this._classCommands = val;
+
+    const origins = new Set();
+    val.forEach(({ extension }) => {
+      origins.add(extension);
+    });
+    this.commandsHaveSameOrigin = origins.size === 1;
+  }
+  get classCommands() {
+    return this._classCommands;
+  }
+
   @Output() view: EventEmitter<{
     command: Command;
     extension: string;

512ee2458114f5ba39ac584dd1b48f9b2462b890
Seemann <mail@sannybuilder.com>
2022-05-14 16:38:55 -0400

    update ci script

--- editor/ci.ts
+++ editor/ci.ts
@@ -7,57 +7,50 @@ const { execSync } = require('child_process');
 const gamesRaw = readFileSync('games.json');
 const games: Game[] = JSON.parse(gamesRaw);
 
+run('npm run generate:support-info dist/editor/assets/support-info.json');
+
+// GENERATE DATA FILES
 games.forEach((game) => {
-  execSync(`npm run validate:commands ../${game}/${game}.json ${game}`, {
-    stdio: [0, 1, 2],
-  });
+  const assets = join('dist', 'editor', 'assets', game);
+  const gameJson = join('../', game, `${game}.json`);
+  const enumsJson = join('../', game, `enums.json`);
 
-  execSync(`npm run validate:enums ../${game}/enums.json`, {
-    stdio: [0, 1, 2],
-  });
+  [
+    `npm run validate:commands ${gameJson} ${game}`,
+    `npm run validate:enums ${enumsJson}`,
+    `[ -d ${assets} ] || mkdir ${assets}`,
 
-  execSync(`mkdir ${join('dist', 'editor', 'assets', game)} -p`, {
-    stdio: [0, 1, 2],
-  });
-
-  execSync(
-    `npm run generate:enums ../${game}/enums.json dist/editor/assets/${game}/enums.js`,
-    {
-      stdio: [0, 1, 2],
-    }
-  );
-
-  execSync(`cp ../${game}/${game}.json dist/editor/assets/${game}`, {
-    stdio: [0, 1, 2],
-  });
-
-  execSync(`cp ../${game}/enums.json dist/editor/assets/${game}`, {
-    stdio: [0, 1, 2],
-  });
+    `npm run generate:enums ${enumsJson} ${join(assets, 'enums.js')}`,
+    `cp ${gameJson} ${assets}`,
+    `cp ${enumsJson} ${assets}`,
+  ].forEach(run);
 
   if (!game.includes('unknown')) {
-    execSync(
-      `cargo run enums ../gta3/enums.json > ../editor/dist/editor/assets/gta3/enums.txt`,
-      {
-        cwd: '../generator',
-        stdio: [0, 1, 2],
-      }
-    );
-
-    execSync(
-      `cargo run classes ../${game}/${game}.json ${game} > ../editor/dist/editor/assets/${game}/classes.db`,
-      {
-        cwd: '../generator',
-        stdio: [0, 1, 2],
-      }
-    );
-
-    execSync(
-      `cargo run keywords ../${game}/${game}.json ${game} > ../editor/dist/editor/assets/${game}/keywords.txt`,
-      {
-        cwd: '../generator',
-        stdio: [0, 1, 2],
-      }
-    );
+    let dest = join('..', 'editor', assets);
+    [
+      `cargo run enums ${enumsJson} > ${join(dest, 'enums.txt')}`,
+      `cargo run classes ${gameJson} ${game} > ${join(dest, 'classes.db')}`,
+      `cargo run keywords ${gameJson} ${game} > ${join(dest, 'keywords.txt')}`,
+    ].forEach(cargo);
   }
 });
+
+// GENERATE SNIPPETS
+['gta3', 'vc', 'sa', 'unknown_x86', 'unknown_x64'].forEach((game) => {
+  const assets = join('..', 'editor', 'dist', 'editor', 'assets', game);
+  const srcDir = join('..', game, 'snippets');
+  cargo(`cargo run snippets ${srcDir} > ${assets}/snippets.json`);
+});
+
+function run(cmd: string) {
+  execSync(cmd, {
+    stdio: [0, 1, 2],
+  });
+}
+
+function cargo(cmd: string) {
+  execSync(cmd, {
+    cwd: '../generator',
+    stdio: [0, 1, 2],
+  });
+}

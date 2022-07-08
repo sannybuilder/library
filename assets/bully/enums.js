8fd5b2567cc61a03a4f7f13d85d77aafb81e612c
user-grinch <user-grinch@users.noreply.github.com>
2022-07-08 19:33:21 +0000

    deploy: d6a424ca876437602364e9ead1aaa25633dd62c6

---
+++ assets/bully/enums.js
@@ -0,0 +1,20 @@
+export const Outfits = {
+  "Starting": "Starting",
+  "Halloween": "Halloween",
+  "BoxingNg": "Boxing NG",
+  "Boxing": "Boxing",
+  "Nutcracker": "Nutcracker",
+  "Mascot": "Mascot",
+  "MascotNoHead": "MascotNoHead",
+  "School": "Uniform",
+  "Orderly": "Orderly",
+  "GymStrip": "Gym Strip",
+  "FastFood": "Fast Food",
+  "Wrestling": "Wrestling",
+  "Underwear": "Underwear"
+};
+export const Fade = {
+  "Out": 0,
+  "In": 1,
+  "None": 2
+};

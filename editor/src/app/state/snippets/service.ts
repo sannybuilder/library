4522eea26eb5840d09476200269dcfb6f9bcc991
Seemann <mail@sannybuilder.com>
2022-12-03 00:39:38 -0500

    update worker config

--- editor/src/app/state/snippets/service.ts
+++ editor/src/app/state/snippets/service.ts
@@ -1,19 +1,14 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Location } from '@angular/common';
 import { Observable } from 'rxjs';
 import { ExtensionSnippets, GameSnippets } from '../../models';
 import { GameEditions } from '../../utils';
+import { GitHubService } from '../github';
 
 @Injectable({ providedIn: 'root' })
 export class SnippetsService {
-  constructor(private http: HttpClient) {}
+  constructor(private _github: GitHubService) {}
 
   loadSnippets(game: keyof typeof GameEditions): Observable<ExtensionSnippets> {
-    const ts = Date.now().toString();
-    return this.http.get<ExtensionSnippets>(
-      Location.joinWithSlash('/assets', GameSnippets[game]),
-      { params: { ts } }
-    );
+    return this._github.loadFileFromAssets(GameSnippets[game]);
   }
 }

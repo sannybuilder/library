cdc2d23e11ef9c903d4639ab897b0409231cf3b0
Seemann <mail@sannybuilder.com>
2024-08-14 13:58:35 -0400

    improve code highlighting

--- editor/src/app/pipes/codify.pipe.ts
+++ editor/src/app/pipes/codify.pipe.ts
@@ -2,6 +2,29 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Command, Extension, Game } from '../models';
 import { template } from 'lodash';
 import { braceify, stringifyWithColon } from './params';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-pascal';
+
+Prism.languages.sb = Prism.languages.extend('pascal', {
+  keyword: [
+    {
+      pattern:
+        /(^|[^&])\b(?:IF|AND|CONST|DOWNTO|ELSE|END|FOR|HEX|NOT|OR|REPEAT|THEN|TO|UNKNOWN|VAR|UNTIL|WHILE|INTEGER|INT|FLOAT|SHORTSTRING|STRING|LONGSTRING|IMPORT|EXPORT|SWITCH|CASE|DEFAULT|FUNCTION|CDECL|STDCALL|THISCALL)\b/i,
+      lookbehind: true,
+    },
+  ],
+  number: [
+    {
+      pattern:
+        /(?:\b0x(?:[\da-f]+(?:\.[\da-f]*)?|\.[\da-f]+)(?:p[+-]?\d+)?|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?)[ful]{0,4}/i,
+    },
+    { pattern: /(^|[^&])\b(?:TRUE|FALSE)\b/i },
+  ],
+  comment: {
+    pattern: /\/\*[\s\S]*?\*\/|\{[\s\S]*?\}|\/\/.*/,
+    greedy: true,
+  },
+});
 
 @Pipe({
   name: 'codify',
@@ -23,7 +46,7 @@ export class CodifyPipe implements PipeTransform {
 
 function normalize(code: string): string {
   // replace tabs with 4 spaces, and angle brackets with html entities
-  return code.replace(/\t/g, '    ').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return code.replace(/\t/g, '    ');
 }
 
 function compileTemplate(code: string, command: Command): string {
@@ -51,8 +74,17 @@ function format(
   showOpcodes: boolean,
   game: Game
 ): string {
-  const lines = code.split('\n').map((line) => line.trimEnd());
+  const highlightName = command.name.startsWith('0x')
+    ? `${command.class}_${command.member}`
+    : command.name;
 
+  const tokenized = Prism.highlight(
+    code,
+    { ...Prism.languages.sb, function: new RegExp(highlightName, 'i') },
+    'sb'
+  );
+
+  const lines = tokenized.split('\n').map((line) => line.trimEnd());
   const linesWithOpcodes = lines.map((line) => {
     const words = line.split(' ');
     const reserved = ['', 'repeat', 'until', 'while', 'if', 'then', 'else'];

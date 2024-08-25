4ec8d0e94a663fbb9fbe87549cec60fe3791d0bf
MiranDMC <MiranDMC@users.noreply.github.com>
2024-08-25 04:53:54 +0000

    deploy: 5a389589149c74b608f1f27bc1bd535fe7bf1e7a

--- assets/gta_iv/enums.js
+++ assets/gta_iv/enums.js
@@ -301,3 +301,12 @@ export const RadarSprite = {
   "Boss": 93,
   "Base": 94
 };
+export const WheelId = {
+  "FrontLeft": 0,
+  "RearLeft": 1,
+  "FrontRight": 2,
+  "RearRight": 3,
+  "Any": 4,
+  "BikeFront": 0,
+  "BikeRear": 1
+};

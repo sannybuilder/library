bc3abb9e683518c66855c25ad5206884a850e314
Seemann <mail@sannybuilder.com>
2024-09-04 09:12:24 -0400

    display function declaration only in native code context

--- editor/src/app/pipes/snippetify.pipe.ts
+++ editor/src/app/pipes/snippetify.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { ViewContext } from '../models';
 
 @Pipe({ name: 'snippetify' })
 export class SnippetifyPipe implements PipeTransform {
   constructor() {}
 
-  transform(content: string) {
-    return `${'<%= decl %>'}\n${content}`;
+  transform(content: string, viewContext: ViewContext) {
+    if (viewContext === ViewContext.Code) {
+      return `${'<%= decl %>'}\n${content}`;
+    }
+    return content;
   }
 }

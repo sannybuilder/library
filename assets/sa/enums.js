e6bfb6b7642b52a9a414f110ddedfe78cb470ffc
MiranDMC <MiranDMC@users.noreply.github.com>
2024-09-14 22:05:29 +0000

    deploy: 2c21b1841453152adde0c2b890a4484c8cc5519f

--- assets/sa/enums.js
+++ assets/sa/enums.js
@@ -2514,17 +2514,6 @@ export const CarLights = {
   "ForceOff": 1,
   "ForceOn": 2
 };
-export const SeatIndex = {
-  "FrontRight": 0,
-  "RearLeft": 1,
-  "RearRight": 2,
-  "Passenger4": 3,
-  "Passenger5": 4,
-  "Passenger6": 5,
-  "Passenger7": 6,
-  "Passenger8": 7,
-  "BikeRear": 0
-};
 export const AudioStreamAction = {
   "Stop": 0,
   "Play": 1,

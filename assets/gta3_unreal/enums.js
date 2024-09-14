7e68c0a9aaee342a42acd0dfffb97bb1b7d64648
MiranDMC <MiranDMC@users.noreply.github.com>
2024-09-14 22:19:20 +0000

    deploy: e72ac02325f4cb393e747511280de34ababe7364

--- assets/gta3_unreal/enums.js
+++ assets/gta3_unreal/enums.js
@@ -885,3 +885,13 @@ export const DrivingMode = {
   "PloughThrough": 3,
   "StopForCarsIgnoreLights": 4
 };
+export const SeatId = {
+  "FrontRight": 0,
+  "RearLeft": 1,
+  "RearRight": 2,
+  "Passenger4": 3,
+  "Passenger5": 4,
+  "Passenger6": 5,
+  "Passenger7": 6,
+  "Passenger8": 7
+};

d40c4d107a485c151943b13ca60bb8bad4570b90
MiranDMC <MiranDMC@users.noreply.github.com>
2024-09-14 22:02:22 +0000

    deploy: f4b104d266cd55b5b12ca142e5ef3d85e796a1d8

--- assets/vc_unreal/enums.js
+++ assets/vc_unreal/enums.js
@@ -1065,3 +1065,14 @@ export const WheelId = {
   "BikeFront": 0,
   "BikeRear": 1
 };
+export const SeatId = {
+  "FrontRight": 0,
+  "RearLeft": 1,
+  "RearRight": 2,
+  "Passenger4": 3,
+  "Passenger5": 4,
+  "Passenger6": 5,
+  "Passenger7": 6,
+  "Passenger8": 7,
+  "BikeRear": 0
+};

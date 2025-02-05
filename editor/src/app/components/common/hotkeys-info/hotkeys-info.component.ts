ea73107432d52e6492d48135e2d4c009d3d1e73d
Seemann <mail@sannybuilder.com>
2025-02-05 13:36:40 -0500

    fix build error

--- editor/src/app/components/common/hotkeys-info/hotkeys-info.component.ts
+++ editor/src/app/components/common/hotkeys-info/hotkeys-info.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'scl-hotkeys-info',
-  imports: [],
   templateUrl: './hotkeys-info.component.html',
   styleUrl: './hotkeys-info.component.scss'
 })

8911cd99778e72c8dc93e8b947a151e5972dc553
Seemann <mail@sannybuilder.com>
2025-05-16 14:54:47 -0400

    display implicit enum values

--- editor/src/app/components/enums/enum-overview/enum-overview.component.ts
+++ editor/src/app/components/enums/enum-overview/enum-overview.component.ts
@@ -1,16 +1,17 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { isStringEnum } from '../../../utils';
+import { fillEnumValues, isStringEnum } from '../../../utils';
 import { EnumRaw, Game } from '../../../models';
 
 @Component({
-    selector: 'scl-enum-overview',
-    templateUrl: './enum-overview.component.html',
-    styleUrls: ['./enum-overview.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: false
+  selector: 'scl-enum-overview',
+  templateUrl: './enum-overview.component.html',
+  styleUrls: ['./enum-overview.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
 })
 export class EnumOverviewComponent {
   private _enumToView: EnumRaw;
+  private _values: EnumRaw['fields'] = [];
   isStringEnum = false;
 
   @Input() game: Game;
@@ -19,8 +20,13 @@ export class EnumOverviewComponent {
   @Input() set enumToView(val: EnumRaw) {
     this._enumToView = val;
     this.isStringEnum = isStringEnum(val.fields);
+    this._values = fillEnumValues(this._enumToView.fields);
   }
   get enumToView() {
     return this._enumToView;
+  }
+
+  autoValue(index: number) {
+    return this._values[index]?.[1] ?? "";
   }
 }

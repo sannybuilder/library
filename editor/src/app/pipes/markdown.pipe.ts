91132b2ee80e164329c5735c55bea382012fc569
Seemann <mail@sannybuilder.com>
2025-08-08 13:11:29 -0400

    update docs

--- editor/src/app/pipes/markdown.pipe.ts
+++ editor/src/app/pipes/markdown.pipe.ts
@@ -2,13 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 import md from 'markdown-it';
 
 @Pipe({
-    name: 'markdown',
-    standalone: false
+  name: 'markdown',
+  standalone: false,
 })
 export class MarkdownPipe implements PipeTransform {
   transform(content: string): string {
     if (content) {
-      return md().render(content);
+      return md({ html: true }).render(content);
     }
     return '';
   }

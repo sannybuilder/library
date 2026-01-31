2179a1d14f2073184f79fcb96445702142bf727b
x87 <x87@users.noreply.github.com>
2026-01-31 21:03:53 +0000

    deploy: a0dc98b1b9b3812a911b93a42fb23554cf274fa8

---
+++ assets/lcs/enums.ts
@@ -0,0 +1,21 @@
+export enum SeatId {
+  "FrontRight" = 0,
+  "RearLeft" = 1,
+  "RearRight" = 2,
+  "Passenger4" = 3,
+  "Passenger5" = 4,
+  "Passenger6" = 5,
+  "Passenger7" = 6,
+  "Passenger8" = 7,
+  "BikeRear" = 0
+};
+export enum SetPieceType {
+  "None" = 0,
+  "TwoCopCarsInAlley" = 1,
+  "CarBlockingPlayerFromSide" = 2,
+  "CarRammingPlayerFromSide" = 3,
+  "CreateCopperOnFoot" = 4,
+  "CreateTwoCoppersOnFoot" = 5,
+  "TwoCarsBlockingPlayerFromSide" = 6,
+  "TwoCarsRammingPlayerFromSide" = 7
+};
---
+++ assets/vcs/enums.ts
@@ -0,0 +1,20 @@
+export enum WheelId {
+  "FrontLeft" = 0,
+  "RearLeft" = 1,
+  "FrontRight" = 2,
+  "RearRight" = 3,
+  "Any" = 4,
+  "BikeFront" = 0,
+  "BikeRear" = 1
+};
+export enum SeatId {
+  "FrontRight" = 0,
+  "RearLeft" = 1,
+  "RearRight" = 2,
+  "Passenger4" = 3,
+  "Passenger5" = 4,
+  "Passenger6" = 5,
+  "Passenger7" = 6,
+  "Passenger8" = 7,
+  "BikeRear" = 0
+};

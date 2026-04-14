2e096ea75a839401e9b09133412fc96056cb85be
Seemann <mail@sannybuilder.com>
2026-04-13 23:32:00 -0400

    update to Angular 21

--- editor/src/app/state/ui/effects.ts
+++ editor/src/app/state/ui/effects.ts
@@ -1,5 +1,5 @@
-import { Inject, Injectable } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable, DOCUMENT } from '@angular/core';
+
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { combineLatest, merge } from 'rxjs';
 import {
